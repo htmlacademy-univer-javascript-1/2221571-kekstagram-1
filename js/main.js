836d6557297a8c9883744b04ef93fe05ec155791
8CloudyCat8 <matveev2464@gmail.com>
2022-09-17 20:23:40 +0500

    added functions

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+function getRandomIntInclusive(min, max)
+{
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < max)
+  {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  else {return 'неверно введённые данные';}
+}
+
+// eslint-disable-next-line no-unused-vars
+function checkingTheMaximumStringLength(originalString, maxLength)
+{
+  if (originalString.length <= maxLength)
+  {
+    return true;
+  }
+  else {return false;}
+}
+
+
