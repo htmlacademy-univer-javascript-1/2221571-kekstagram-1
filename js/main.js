d834662c30a2939690f53933127821a252d67f7e
8CloudyCat8 <matveev2464@gmail.com>
2022-09-20 20:56:40 +0500

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,23 +1,15 @@
 // eslint-disable-next-line no-unused-vars
-function getRandomIntInclusive(min, max)
-{
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min < max)
-  {
+  if (min < max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    return 'неверно введённые данные';
   }
-  else {return 'неверно введённые данные';}
 }
 
 // eslint-disable-next-line no-unused-vars
-function checkingTheMaximumStringLength(originalString, maxLength)
-{
-  if (originalString.length <= maxLength)
-  {
-    return true;
-  }
-  else {return false;}
+function checkMaximumStringLength(originalString, maxLength) {
+  return originalString.length <= maxLength;
 }
-
-
